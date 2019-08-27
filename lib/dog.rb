2d6376ee5fca8e7b1218b62eb4a1ac79e926bd8c
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-27 20:24:37 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+  def initialize(dog_name)
+  @name = fido = Dog.new
+fido.name = "Fido"
+  end
+  # def name
+  #   @name
+  # end
+end 
+# fido = Dog.new
+# fido.name = "Fido"
+# puts fido.name
